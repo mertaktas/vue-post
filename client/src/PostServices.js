61613e9ae1a0dda20794c8ee9197ca20fd35889a
mert <mertaktas231@gmail.com>
2019-12-16 01:38:02 +0300

    vue.config eklendi.

--- client/src/PostServices.js
+++ client/src/PostServices.js
@@ -1,35 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
 
-const url = 'http://localhost:5000/api/posts/';
+const url = "api/posts/";
 
 class PostService {
-    // Get Post
-    static getPosts() {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const res = await axios.get(url);
-                const data = res.data;
-                resolve(
-                    data.map(post => ({
-                        ...post,
-                        createdAt: new Date(post.createdAt)
-                    }))
-                )
-            } catch (err) {
-                reject(err);
-            }
-        });
-    }
-    // Create Post
-    static insertPost(text) {
-        return axios.post(url, {
-            text
-        });
-    }
-    // Delete Post
-    static deletePost(id) {
-        return axios.delete(`${url}${id}`);
-    }
+  // Get Post
+  static getPosts() {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        resolve(
+          data.map(post => ({
+            ...post,
+            createdAt: new Date(post.createdAt)
+          }))
+        );
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+  // Create Post
+  static insertPost(text) {
+    return axios.post(url, {
+      text
+    });
+  }
+  // Delete Post
+  static deletePost(id) {
+    return axios.delete(`${url}${id}`);
+  }
 }
 
 export default PostService;
