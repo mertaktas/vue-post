70ccbae9551c083b241a6b254ca14ff8eea5ea7d
mert <mertaktas231@gmail.com>
2019-12-15 22:59:38 +0300

    posts.js ayarlandı. Get,post,delete ayarlandı. Mongodb eklendi.

---
+++ server/routes/api/posts.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get post
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray());
+});
+// Add post
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
+// Delete post
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({
+        _id: new mongodb.ObjectID(req.params.id)
+    });
+    res.status(200).send();
+});
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect('mongodb+srv://mert123:mert123@mevn-zgttp.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+
+    return client.db('mevn_post').collection('posts');
+}
+
+module.exports = router;
